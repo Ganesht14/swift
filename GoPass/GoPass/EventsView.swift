be3cf59af6e90046c8cadc474136270efdb3e2ff
reshmaresh02 <reshmukh2003@gmail.com>
2024-07-11 14:46:48 +0530

    code changes

--- GoPass/GoPass/EventsView.swift
+++ GoPass/GoPass/EventsView.swift
@@ -8,46 +8,49 @@
 import SwiftUI
 
 struct EventsView: View {
-               var body: some View {
-                   VStack {
-                       Text("Events")
-                     ScrollView(.horizontal) {
-                           HStack(spacing: 10) {
-                               ForEach(1..<4) { index in
-                                   Button(action: {
-                                       print("Box \(index) tapped")
-                                   }) {
+    var body: some View {
+        VStack(alignment:.leading){
+            Text("Events")
+                .font(.title3)
+                .foregroundColor(.black)
+                .padding(.leading,15)
+            ScrollView(.horizontal) {
+                HStack(spacing: 10) {
+                    ForEach(1..<4) { index in
+                        Button(action: {
+                            print("Box \(index) tapped")
+                        }) {
                             VStack(spacing: 0){
-                                           Image("eventImage\(index)")
+                                Image("eventImage\(index)")
                                     .resizable()
                                     .aspectRatio(contentMode: .fill)
                                     .frame(width: 200, height: 100)
                                     .cornerRadius(20)
                                 
-                            Text("Nov 09")
+                                Text("Nov 09")
                                     .foregroundColor(Color.gray)
                                     .padding([.top, .leading], 10)
-                            Text(getDescription(for: index))
+                                Text(getDescription(for: index))
                                     .foregroundColor(Color.blue)
                                     .fontWeight(.medium)
-.padding([.top, .leading, .trailing], 10)
-                                       .padding(.bottom, 70)
+                                    .padding([.top, .leading, .trailing], 10)
+                                    .padding(.bottom, 70)
+                            }
+                            .background(Color.white)
+                            .cornerRadius(20)
+                            .shadow(radius: 5)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 20)
+                                    .stroke(Color.gray, lineWidth: 1) // Adjust border color and width as needed
+                            )
+                        }
+                        .buttonStyle(PlainButtonStyle())
                     }
-                                       .background(Color.white)
-                                       .cornerRadius(20)
-                                       .shadow(radius: 5)
-                                       .overlay(
-                                                            RoundedRectangle(cornerRadius: 20)
-                                                                      .stroke(Color.gray, lineWidth: 1) // Adjust border color and width as needed
-                                                              )
-                                   }
-                                   .buttonStyle(PlainButtonStyle())
-                               }
-                           }
-                           .padding(.leading, 10)
-                       }
-                   }
-                   
+                }
+                .padding(.leading, 10)
+            }
+        }
+        
     }
     func getDescription(for index: Int) -> String {
         switch index {
